68363dbb961e6172d35d87961d6eef48cb53a7fa
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-20 15:43:40 +0900

    refactor(input_page): コード修正

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,8 +12,44 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('ダークテーマ使用中'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  color: Colors.white70,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+          )),
+          Expanded(
+            child: ReusableCard(
+              color: Colors.white70,
+            ),
+          ),
+          Expanded(
+              child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ReusableCard(
+                  color: Colors.white70,
+                ),
+              ),
+              Expanded(
+                child: ReusableCard(
+                  color: Colors.white70,
+                ),
+              ),
+            ],
+          )),
+        ],
       ),
       floatingActionButton: Theme(
         data: ThemeData(accentColor: Colors.orange),
@@ -21,6 +57,23 @@ class _InputPageState extends State<InputPage> {
           child: Icon(Icons.add),
         ),
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({@required this.color});
+
+  final Color color;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: color, borderRadius: BorderRadius.circular(10.0)),
+      width: 170.0,
+      height: 200.0,
+      child: Text('ダークテーマ使用中'),
     );
   }
 }
