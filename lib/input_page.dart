3c0bdc6db6bfb3802be5ebe0d553c2ccb1509681
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-21 04:04:13 +0900

    feat(input_page, results_page): router追加

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,10 @@
+import 'package:calculatorapp/results_page.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'icon_content.dart';
 import 'reusable_card.dart';
 import 'consts.dart';
+import 'results_page.dart';
 
 enum Gender { male, female }
 
@@ -205,11 +207,18 @@ class _InputPageState extends State<InputPage> {
               ),
             ],
           )),
-          Container(
-            color: kBottomContainerColor,
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: kBottomContainerHeight,
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ResultsPage()));
+            },
+            child: Container(
+              child: Text('CALCULATE'),
+              color: kBottomContainerColor,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: kBottomContainerHeight,
+            ),
           )
         ],
       ),
