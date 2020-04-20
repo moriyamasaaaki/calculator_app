61d615951ef4116a56c5e0c42854cc4d9dbb902d
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-20 17:41:34 +0900

    feat(input_page): dartファイル分割、icon追加

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'icon_content.dart';
+import 'reusable_card.dart';
 
 const bottomContainerHeight = 80.0;
-const activeCardColor = Colors.white70;
+const activeCardColor = Colors.teal;
 const bottomContainerColor = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
@@ -24,11 +27,19 @@ class _InputPageState extends State<InputPage> {
               Expanded(
                 child: ReusableCard(
                   color: activeCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    label: 'MARS',
+                  ),
                 ),
               ),
               Expanded(
                 child: ReusableCard(
                   color: activeCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    label: 'FEMALE',
+                  ),
                 ),
               ),
             ],
@@ -67,24 +78,6 @@ class _InputPageState extends State<InputPage> {
           child: Icon(Icons.add),
         ),
       ),
-    );
-  }
-}
-
-class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color});
-
-  final Color color;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: color, borderRadius: BorderRadius.circular(10.0)),
-      width: 170.0,
-      height: 200.0,
-      child: Text('ダークテーマ使用中'),
     );
   }
 }
