56bb34e1089b997f30c153e5be6e3c8c210bf8e4
moriyamasaaaki <moriya-7071@outlook.com>
2020-04-20 19:27:17 +0900

    refactor(input_page, reusable_card): コードrefactor

--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.color, this.cardChild});
+  ReusableCard({@required this.color, this.cardChild, this.onPress});
 
   final Color color;
   final Widget cardChild;
+  final Function onPress;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: cardChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-          color: color, borderRadius: BorderRadius.circular(10.0)),
-      width: 170.0,
-      height: 200.0,
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: cardChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            color: color, borderRadius: BorderRadius.circular(10.0)),
+        width: 170.0,
+        height: 200.0,
 //      child: Text('ダークテーマ使用中'),
+      ),
     );
   }
 }
